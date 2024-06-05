3a5a29252535d4023c3ab17ff736d577c553062b
gabrielsouzaleite <gabriel.s.leite05@gmail.com>
2024-06-05 14:20:00 -0300

    Projeto em construção :construction:

--- src/use-cases/check-in.spec.ts
+++ src/use-cases/check-in.spec.ts
@@ -1,21 +1,103 @@
-import { expect, describe, it, beforeEach } from 'vitest'
+import { afterEach, expect, describe, it, beforeEach, vi } from 'vitest'
 import { InMemoryCheckInsRepository } from '@/repositories/in-memory/in-memory-check-ins-repository'
 import { CheckInUseCase } from './check-in'
+import { InMemoryGymsRepository } from '@/repositories/in-memory/in-memory-gyms-repository'
+import { Decimal } from '@prisma/client/runtime/library'
+import { MaxNumberCheckInsError } from './errors/max-number-off-check-in-error'
+import { MaxDistanceError } from './errors/max-distance-error'
 
 let checkInsRepository: InMemoryCheckInsRepository
+let gymsRepository: InMemoryGymsRepository
 let sut: CheckInUseCase
 describe('Register Use Case', () => {
-  beforeEach(() => {
+  beforeEach( async () => {
     checkInsRepository = new InMemoryCheckInsRepository()
-    sut = new CheckInUseCase(checkInsRepository)
+    gymsRepository = new InMemoryGymsRepository()
+    sut = new CheckInUseCase(checkInsRepository, gymsRepository)
+
+    await gymsRepository.create({
+      id: 'gym-01',
+      title: 'Smart Fit',
+      description: '',
+      phone: '',
+      latitude: -23.4705743,
+      longitude: -46.6029906
+    })
+
+    vi.useFakeTimers()
+  })
+
+  afterEach(() => {
+    vi.useRealTimers()
   })
 
   it('should be able to check in', async () => {
+  
     const { checkIn } = await sut.execute({
       gymId: 'gym-01',
-      userId: 'user-01'
+      userId: 'user-01',
+      userLatitude: -23.4705743,
+      userLongitude: -46.6029906
     })
 
     expect(checkIn.id).toEqual(expect.any(String))
+  })
+
+  it('should not be able to check in twice in the same day', async () => {
+    vi.setSystemTime(new Date(2022, 0, 20, 8, 0, 0))
+
+    await sut.execute({
+      gymId: 'gym-01',
+      userId: 'user-01',
+      userLatitude: -23.4705743,
+      userLongitude: -46.6029906
+    })
+
+    await expect(() => sut.execute({
+      gymId: 'gym-01',
+      userId: 'user-01',
+      userLatitude: -23.4705743,
+      userLongitude: -46.6029906
+    })).rejects.toBeInstanceOf(MaxNumberCheckInsError)
+  })
+
+  it('should not be able to check in twice but in different days', async () => {
+    vi.setSystemTime(new Date(2022, 0, 20, 8, 0, 0))
+
+    await sut.execute({
+      gymId: 'gym-01',
+      userId: 'user-01',
+      userLatitude: -23.4705743,
+      userLongitude: -46.6029906
+    })
+
+    vi.setSystemTime(new Date(2022, 0, 21, 8, 0, 0))
+
+    const { checkIn } = await sut.execute({
+      gymId: 'gym-01',
+      userId: 'user-01',
+      userLatitude: -23.4705743,
+      userLongitude: -46.6029906
+    })
+
+    expect(checkIn.id).toEqual(expect.any(String))
+  })
+
+  it('should not be able to check in on distant gym', async () => {
+    gymsRepository.items.push({
+      id: 'gym-02',
+      title: 'Gym 01',
+      description: 'Rua 01, 01',
+      phone: '',
+      latitude: new Decimal(-23.4927758),
+      longitude: new Decimal(-46.5963995)
+    })
+
+    await expect(() => sut.execute({
+      gymId: 'gym-02',
+      userId: 'user-01',
+      userLatitude: -23.5160124,
+      userLongitude: -46.4584091
+    })).rejects.toBeInstanceOf(MaxDistanceError)
   })
 })
---
+++ src/use-cases/create-gym.spec.ts
@@ -0,0 +1,24 @@
+import { expect, describe, it, beforeEach } from 'vitest'
+import { InMemoryGymsRepository } from '@/repositories/in-memory/in-memory-gyms-repository'
+import { CreateGymUseCase } from './create-gym'
+
+let gymsRepository: InMemoryGymsRepository
+let sut: CreateGymUseCase
+describe('Create Use Case', () => {
+  beforeEach(() => {
+    gymsRepository = new InMemoryGymsRepository()
+    sut = new CreateGymUseCase(gymsRepository)
+  })
+
+  it('should be able to create gym', async () => {
+    const { gym } = await sut.execute({
+      title: 'Smart Fit',
+      description: null,
+      phone: null,
+      latitude: -23.4705743,
+      longitude: -46.6029906
+    })
+
+    expect(gym.id).toEqual(expect.any(String))
+  })
+})
